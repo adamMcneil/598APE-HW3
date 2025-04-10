8d7d73c210f7b11a976977c95a38dc10220e648d
adamwm4 <adamwm4@illinois.edu>
2025-04-10 16:46:57 -0500

    made the function void

--- graph.py
+++ graph.py
@@ -3,25 +3,26 @@ import numpy as np
 # ./main.exe 1000 5000
 # final location 4221.875063 -2827.830995
 base_line_mid = [12.486, 12.30, 12.32]
-reduce_work_mid = [8.207]
+copy = [11.90, 12.39, 12.17]
 rust_rw = [7.85, 8.05, 7.93]
 masses = [7.65, 7.80]
 init = [5.399, 5.506]
-mid_data = [base_line_mid, reduce_work_mid, masses, init]
+mid_data = [base_line_mid, copy, masses, init]
+
 
 # ./main.exe 5 100000000
 # final location -246420916.320709 327085378.139325
 base_line_small = [8.774, 8.33, 8.42]
-reduce_work_small = [4.944]
+reduce_work_small = [6.63, 6.93, 6.61]
 rust_rw = [5.27, 5.33]
 masses = [4.95, 5.11]
 init = [5.23, 5.15]
 small_data = [base_line_small, reduce_work_small, masses, init]
 
 # ./main.exe 1000 10000
-# final location -763.433705 1203.599962
+# final location 9740.442359 -5365.206700
 base_line_large = [25.872, 24.72, 24.56]
-reduce_work_large = [14.223737]
+reduce_work_large = [24.03, 24.79]
 rust_rw = [15.93, 16.56]
 masses = [16.34, 16.38]
 init = [11.02, 11.08]
